10cd71a8bc3c0a73d3845d6069eea16079754626
walker277 <146030713+walker277@users.noreply.github.com>
2025-09-05 13:57:56 +0200

    Update Run.sh

--- Run.sh
+++ Run.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
+if [ ! -x ./Build.sh ]; then
+    chmod +x ./Build.sh
+fi
+
+if [ ! -x ./Makedoc.sh ]; then
+    chmod +x ./Makedoc.sh
+fi
+
 ./Build.sh
 if [ $? -ne 0 ]; then
   echo "Build failed, exiting."
   exit 1
 fi
+
 ./Makedoc.sh
 if [ $? -ne 0 ]; then
   echo "Javadoc generation failed, continuing to run the program."
 fi
+
 java -cp ./bin Main "$@"
